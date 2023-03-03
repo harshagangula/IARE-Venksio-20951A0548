269464e78733e1e57e6ffca164535be76197c02c
harshagangula <126311737+harshagangula@users.noreply.github.com>
2023-03-03 12:20:02 +0530

    Update multithreading_java.java

--- task1/multithreading_java.java
+++ task1/multithreading_java.java
@@ -11,26 +11,21 @@ public class ReverseParagraph {
         List<Thread> threads = new ArrayList<>();
         List<String> reversedStrings = new ArrayList<>();
 
-        // Split the paragraph into a list of strings
         String[] strings = paragraph.split(" ");
 
-        // Create a thread for each string and append it to the thread list
         for (String s : strings) {
             Thread t = new Thread(() -> reversedStrings.add(reverseString(s)));
             threads.add(t);
             t.start();
         }
 
-        // Wait for all threads to finish
         for (Thread t : threads) {
             t.join();
         }
 
-        // Join the reversed strings back into a paragraph
         return String.join(" ", reversedStrings);
     }
 
-    // Example usage
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter:");
